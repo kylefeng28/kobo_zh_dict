6f9d0d0cb95bbae72bf3bf8295ef3ddb4f216d17
Kyle Feng <kylefeng28@gmail.com>
2026-08-11 16:46:22 -0700

    Add Traditional and Zhuyin modes

--- src/generic_dict.rs
+++ src/generic_dict.rs
@@ -1,4 +1,5 @@
 use crate::cedict;
+use pinyin_zhuyin::{encode_pinyin, encode_zhuyin};
 
 pub struct Entry {
     // The integer here is a very rough priority ranking indicating
@@ -8,23 +9,37 @@ pub struct Entry {
     pub definition: String,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
+pub enum CharacterMode {
+    Traditional,
+    Simplified,
+}
+
+#[derive(Debug, Clone)]
+pub enum PronunciationMode {
+    Pinyin,
+    Zhuyin,
+}
+
+#[derive(Debug, Clone)]
 pub struct EntrySettings {
     /// Will add a horizontal bar at the top of each entry.  This is mainly for
     /// Kobo, which displays all entries together in a continuous page.
     pub add_separators: bool,
+    pub character_mode: CharacterMode,
+    pub pronunciation_mode: PronunciationMode,
 }
 
 pub fn generate_entries(
     cedict_entries: &Vec<cedict::DictEntry>,
-    entry_settings: EntrySettings,
+    entry_settings: &EntrySettings,
 ) -> Vec<Entry> {
     let mut entries = Vec::new();
 
     for entry in cedict_entries.iter() {
         entries.push(Entry {
             keys: generate_keys(&entry),
-            definition: generate_definition_text(&entry, entry_settings),
+            definition: generate_definition_text(&entry, &entry_settings),
         });
     }
 
@@ -41,9 +56,43 @@ fn generate_keys(entry: &cedict::DictEntry) -> Vec<(String, u32)> {
     keys
 }
 
+fn get_character_variants<'a>(
+    entry: &'a cedict::DictEntry,
+    entry_settings: &EntrySettings,
+) -> (&'a str, &'a str) {
+    match entry_settings.character_mode {
+        CharacterMode::Traditional => (entry.traditional(), entry.simplified()),
+        CharacterMode::Simplified => (entry.simplified(), entry.traditional()),
+    }
+}
+
+fn get_pronunciation(
+    entry: &cedict::DictEntry,
+    entry_settings: &EntrySettings,
+) -> String {
+    // CC-CEDICT entry is raw pinyin with numbers
+    let raw_pinyin = entry.pinyin().to_string();
+    let encode = match entry_settings.pronunciation_mode {
+        PronunciationMode::Pinyin => encode_pinyin,
+        PronunciationMode::Zhuyin => encode_zhuyin,
+    };
+
+    raw_pinyin.split_whitespace()
+        .map(|syllable| {
+            match encode(syllable) {
+                Some(encoded) => encoded,
+                None => {
+                    println!("warning: invalid pinyin '{}' in '{}'", syllable, raw_pinyin);
+                    syllable.to_string()
+                }
+            }
+        })
+        .collect::<String>()
+}
+
 fn generate_definition_text(
     entry: &cedict::DictEntry,
-    entry_settings: EntrySettings,
+    entry_settings: &EntrySettings,
 ) -> String {
     let mut text = String::new();
     if entry_settings.add_separators {
@@ -54,18 +103,17 @@ fn generate_definition_text(
     // Need to force the default Kobo Chinese font, otherwise simplified or Chinese-only characters will not be displayed at all
     let font_style = "style=\"font-family: 'AR UDJingxihei' !important;\"";
     text.push_str(format!("<div {}>", font_style).as_str());
-    let default = entry.simplified();
+    let (preferred, alt) = get_character_variants(entry, entry_settings);
     text.push_str("<br>");
-    text.push_str(default);
-    let alt = entry.traditional();
-    if default != alt {
+    text.push_str(preferred);
+    if preferred != alt {
         text.push_str(" 【");
         text.push_str(alt);
         text.push_str("】");
     }
     text.push_str("</b> ");
 
-    text.push_str(entry.pinyin());
+    text.push_str(get_pronunciation(entry, entry_settings).as_str());
 
     text.push_str("<br/>");
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use clap::{Command, arg};
 
 mod kobo;
 mod generic_dict;
@@ -6,20 +7,63 @@ mod cedict;
 
 use crate::generic_dict::{generate_entries, EntrySettings};
 
+// Need to output as a Japanese dictionary even though this is for Chinese,
+// since Kobo has specially handling for kanji/hanzi characters but only if the locale is "ja"
+const DEFAULT_OUTPUT: &str = "dicthtml-ja-en.zip";
+const DEFAULT_CEDICT: &str = "cedict_1_0_ts_utf-8_mdbg.txt";
+
 fn main() -> io::Result<()> {
-    let cedict_entries = cedict::parse_file("./cedict_1_0_ts_utf-8_mdbg.txt")?;
+    let matches = Command::new("kobo_zh_dict")
+        .about("Kobo Chinese Dictionary Builder")
+        .arg(arg!(--cedict <CEDICT> "Path to CC-CEDICT file")
+            .default_value(DEFAULT_CEDICT)
+        )
+        .arg(arg!(--output <OUTPUT> "The output filepath to write the new dictionary to.")
+            .default_value(DEFAULT_OUTPUT)
+        )
+        .arg(arg!(-c --character <CHARACTER_MODE> "Character mode")
+            .value_parser(["traditional", "simplified"])
+            .default_value("simplified")
+        )
+        .arg(arg!(-p --pronunciation <PRONUNCIATION_MODE> "Pronunciation mode")
+            .value_parser(["pinyin", "zhuyin"])
+            .default_value("pinyin")
+        )
+        .get_matches();
 
-    // Need to output as a Japanese dictionary even though this is for Chinese,
-    // since Kobo has specially handling for kanji/hanzi characters but only if the locale is "ja"
-    let output_filename = "dicthtml-ja-en.zip";
+    // Output zip archive path.
+    let cedict_filename = matches.get_one::<String>("cedict").unwrap();
+    let output_filename = matches.get_one::<String>("output").unwrap();
+    let character_mode = match matches.get_one::<String>("character").unwrap().as_str() {
+        "traditional" => generic_dict::CharacterMode::Traditional,
+        "simplified" => generic_dict::CharacterMode::Simplified,
+        _ => unreachable!()
+    };
+    let pronunciation_mode = match matches.get_one::<String>("pronunciation").unwrap().as_str() {
+        "pinyin" => generic_dict::PronunciationMode::Pinyin,
+        "zhuyin" => generic_dict::PronunciationMode::Zhuyin,
+        _ => unreachable!()
+    };
 
-    let entry_settings = EntrySettings { add_separators: true };
+    let cedict_entries = cedict::parse_file(cedict_filename)?;
 
-    let entries = generate_entries(&cedict_entries, entry_settings);
+    let entry_settings = EntrySettings {
+        add_separators: true,
+        character_mode: character_mode.clone(),
+        pronunciation_mode: pronunciation_mode.clone(),
+    };
 
+    let entries = generate_entries(&cedict_entries, &entry_settings);
 
-    println!("Writing Kobo dictionary to disk...");
+    println!("Using CC-CEDICT file at {}", cedict_filename);
+    println!("Will output Kobo dictionary to {}", output_filename);
+    println!("Character mode: {:?}", character_mode);
+    println!("Pronunciation mode: {:?}", pronunciation_mode);
+    println!();
+
+    println!("Generating Kobo dictionary...");
     kobo::write_dictionary(&entries, std::path::Path::new(output_filename))?;
+    println!("Wrote Kobo dictionary to {}", output_filename);
 
     Ok(())
 }
